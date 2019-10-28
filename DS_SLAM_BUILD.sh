4b8f7b82adb8d72fa388255c8af2ecd2cc461e22
gaunthan <gaunthanhuang@foxmail.com>
2019-10-28 12:19:26 +0800

    Decrease the number of threads that 'make' used; export ROS_PACKAGE_PATH for building the example

--- DS_SLAM_BUILD.sh
+++ DS_SLAM_BUILD.sh
@@ -5,7 +5,7 @@ cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j12
+make -j8
 
 cd ../../g2o
 
@@ -14,7 +14,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j12
+make -j8
 
 cd ../../../
 
@@ -24,7 +24,7 @@ cd Examples/ROS/ORB_SLAM2_PointMap_SegNetM/libsegmentation
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j12
+make -j8
 
 cd ../../../../../
 
@@ -33,7 +33,7 @@ echo "Configuring and building libORB_SLAM2_PointMap_SegNetM.so ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j12
+make -j8
 
 cd ..
 
@@ -41,10 +41,12 @@ cd ..
 echo "Configuring and building Executable TUM ..."
 
 cd Examples/ROS/ORB_SLAM2_PointMap_SegNetM
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:`pwd`/../
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j12
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+make -j8
 
 cd ../../../../
 
